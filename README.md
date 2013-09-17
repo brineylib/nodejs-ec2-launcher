32b6158696c27c630a663ebcba430a3e61a85675
Matthias Geisler <matthias@semanticdreamer.com>
2013-09-17 14:12:34 +0200

    basic express app skeleton with Persona auth and Hogan/ Mustache tpl going

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-nodejs-ec2-launcher
-===================
+# nodejs-ec2-launcher
 
 Node.js web application to start and stop AWS EC2 instances
+
+## Building Blocks
+
+### Server
+
+[express](http://expressjs.com/) - web application framework for node 
+
+    $ express --sessions --css less --hogan --force nodejs-ec2-launcher
+
+[hogan-express](https://npmjs.org/package/hogan-express) - [Hogan.js](http://twitter.github.io/hogan.js/),  [Mustache](http://mustache.github.io/) template engine for express 3.x. Support partials and layout.
+
+    $ npm install hogan-express --save
+
+[Passport](http://passportjs.org/) - Simple, unobtrusive authentication for Node.js.
+
+    $ npm install passport --save
+
+[Passport-BrowserID](https://github.com/jaredhanson/passport-browserid) - Passport strategy for authenticating with [Mozilla Persona](https://login.persona.org/) (formerly known as BrowserID).
+
+    $ npm install passport-browserid --save
+
+[AWS SDK für Node.js](https://aws.amazon.com/de/sdkfornodejs/)
+
+    $ npm install aws-sdk --save
+
+### Client
+
+[zepto.js](http://zeptojs.com/) - a minimalist JavaScript library for modern browsers with a largely jQuery-compatible API. 
+
+## Start the app
+
+    $ foreman start
+
+Point your favourite browser to [http://localhost:3000/](http://localhost:3000/).
+
+Default PORT is `3000`. To change create `.env` file with e.g. `PORT=3001`.
---
+++ app.js
@@ -0,0 +1,119 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var passport = require('passport');
+var util = require('util');
+var BrowserIDStrategy = require('passport-browserid').Strategy;
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the BrowserID verified email address
+//   is serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(email, done) {
+  done(null, { email: email });
+});
+
+
+// Use the BrowserIDStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, a BrowserID verified email address), and invoke
+//   a callback with a user object.
+passport.use(new BrowserIDStrategy({
+    audience: 'http://' + (process.env.HOST || 'localhost') + ':' + (process.env.PORT || '3000')
+  },
+  function(email, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's email address is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the email address with a user record in your database, and
+      // return that user instead.
+      return done(null, { email: email })
+    });
+  }
+));
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.set('layout', 'layout'); // use layout.html as the default layout
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// POST /auth/persona
+// Use passport.authenticate() as route middleware to authenticate the
+// request.  BrowserId authentication will verify the assertion obtained from
+// the browser via the JavaScript API.
+app.post('/auth/persona', 
+  passport.authenticate('browserid', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Simple route middleware to ensure user is authenticated.
+// Use this route middleware on any resource that needs to be protected.  If
+// the request is authenticated (typically via a persistent login session),
+// the request will proceed.  Otherwise, the user will be redirected to the
+// login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
