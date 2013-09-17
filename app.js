bd259d501684cc0f089fe65298414152feb77d94
Matthias Geisler <matthias@semanticdreamer.com>
2013-09-17 14:15:56 +0200

    code comment formatting

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ var util = require('util');
 var BrowserIDStrategy = require('passport-browserid').Strategy;
 
 // Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the BrowserID verified email address
-//   is serialized and deserialized.
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session.  Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.  However, since this example does not
+// have a database of user records, the BrowserID verified email address
+// is serialized and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user.email);
 });
@@ -29,9 +29,9 @@ passport.deserializeUser(function(email, done) {
 
 
 // Use the BrowserIDStrategy within Passport.
-//   Strategies in passport require a `validate` function, which accept
-//   credentials (in this case, a BrowserID verified email address), and invoke
-//   a callback with a user object.
+// Strategies in passport require a `validate` function, which accept
+// credentials (in this case, a BrowserID verified email address), and invoke
+// a callback with a user object.
 passport.use(new BrowserIDStrategy({
     audience: 'http://' + (process.env.HOST || 'localhost') + ':' + (process.env.PORT || '3000')
   },
@@ -91,7 +91,7 @@ app.get('/login', function(req, res){
 
 // POST /auth/persona
 // Use passport.authenticate() as route middleware to authenticate the
-// request.  BrowserId authentication will verify the assertion obtained from
+// request. BrowserId authentication will verify the assertion obtained from
 // the browser via the JavaScript API.
 app.post('/auth/persona', 
   passport.authenticate('browserid', { failureRedirect: '/login' }),
@@ -109,9 +109,9 @@ http.createServer(app).listen(app.get('port'), function(){
 });
 
 // Simple route middleware to ensure user is authenticated.
-// Use this route middleware on any resource that needs to be protected.  If
+// Use this route middleware on any resource that needs to be protected. If
 // the request is authenticated (typically via a persistent login session),
-// the request will proceed.  Otherwise, the user will be redirected to the
+// the request will proceed. Otherwise, the user will be redirected to the
 // login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
